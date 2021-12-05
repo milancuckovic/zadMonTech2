508cbe68875b33d92f505a4dd9bc73b6f58e4be1
milancuckovic <milancuckovic94@gmail.com>
2021-12-06 00:51:02 +0100

    final

--- src/App.js
+++ src/App.js
@@ -3,39 +3,76 @@ import RepoList from "./components/RepoList";
 import User from "./components/User";
 
 class App extends React.Component{
+    
     state={
         user:"milancuckovic",
         userData:[],
         repos:[],
         stars:0,
+        error:""
     }
     
    
+ 
+    
     componentDidMount(){
-        fetch(`https://api.github.com/users/${this.state.user}`)
+        
+        fetch(`https://api.github.com/users/milancuckovic`,)
         .then(res=>{return res.json();})
         .then(data=>{
             this.setState({userData:data})
-        })
-        fetch(`https://api.github.com/users/${this.state.user}/starred`)
+        });
+        fetch(`https://api.github.com/users/milancuckovic/starred`)
         .then(res=>{return res.json();})
         .then(data=>{
             this.setState({stars:data.length})
         })
-        fetch(`https://api.github.com/users/${this.state.user}/repos`)
+        fetch(`https://api.github.com/users/milancuckovic/repos`)
         .then(res=>{return res.json();})
         .then(data=>{
             this.setState({repos:data})
         })
     }
-    
-
   
+    setSearch=(e)=>{
+        this.setState({user:e.target.value})
+    }
+    setError(mes){
+        this.setState({error:mes});
+    }
     Search=(e)=>{
         e.preventDefault();
         console.log(this.state.user)
-        this.setState({user:e.target.value})
-        console.log(this.state.user)
+        fetch(`https://api.github.com/users/${this.state.user}`)
+        .then(res=>{return res.json();})
+        .then(data=>{
+            if(data.message){
+                this.setError(data.message)
+            }
+            else{
+                this.setState({userData:data})
+            }
+        })
+        fetch(`https://api.github.com/users/${this.state.user}/starred`)
+        .then(res=>{return res.json();})
+        .then(data=>{
+            if(data.message){
+               this.setError(data.message)
+            }
+            else{
+                this.setState({stars:data.length})
+            }
+        })
+        fetch(`https://api.github.com/users/${this.state.user}/repos`)
+        .then(res=>{return res.json();})
+        .then(data=>{
+            if(data.message){
+                this.setError(data.message)
+            }
+            else{
+                this.setState({repos:data})
+            }
+        })
     }
 
 
@@ -46,10 +83,11 @@ class App extends React.Component{
                     <a href="/" className="navbar-brand">Home</a>
                     
                         <form action="" onSubmit={this.Search} className="form-inline">
-                            <input type="search" className="form-control" placeholder="Users..."></input>
-                            <button type="submit" className="btn btn-dark" onClick={this.Search}><i className="fa fa-search"></i></button>
+                            <input type="search" className="form-control" placeholder="Users..." onChange={this.setSearch}></input>
+                            <button type="submit" className="btn btn-dark" ><i className="fa fa-search"></i></button>
                         </form>
                 </nav>
+                { this.state.error  ? (<h1>{this.state.error}</h1>) :
                 <div className="container mt-4">
                     <div className="row">
                         <div className="col-md-4">
@@ -59,7 +97,8 @@ class App extends React.Component{
                             <RepoList repos={this.state.repos}/>
                         </div>
                     </div>
-                </div>  
+                </div> }
+                 
             </>
         )
     }
